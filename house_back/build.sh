8a8a6741b708cbcf256acb249b4e1ec7471822b2
Gearup120 <gearup002211@gmail.com>
2026-04-29 21:58:29 +0100

    Ensure superuser is email verified

--- house_back/build.sh
+++ house_back/build.sh
@@ -8,7 +8,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Auto-create superuser if it doesn't exist
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='super@nestverify.com').exists() or User.objects.create_superuser('super@nestverify.com', 'SuperPassword123!', first_name='Super', last_name='Admin')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u, _ = User.objects.get_or_create(email='super@nestverify.com', defaults={'first_name':'Super', 'last_name':'Admin', 'is_staff':True, 'is_superuser':True, 'role':'admin', 'is_email_verified':True}); u.set_password('SuperPassword123!'); u.is_email_verified=True; u.save()"
 
 # Run the mixed seeder
 python seed_mixed.py
