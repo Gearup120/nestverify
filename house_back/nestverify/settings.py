b439c67cef2004c7b1ecece46dfbe83b7fc63618
Gearup120 <gearup002211@gmail.com>
2026-04-30 10:04:17 +0100

    Fix Render build error by adding defaults to email config

--- house_back/nestverify/settings.py
+++ house_back/nestverify/settings.py
@@ -175,7 +175,7 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT          = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS       = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER     = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_TIMEOUT       = 3
-DEFAULT_FROM_EMAIL  = f'NestVerify <{config("EMAIL_HOST_USER")}>'
+DEFAULT_FROM_EMAIL  = f'NestVerify <{EMAIL_HOST_USER}>'
